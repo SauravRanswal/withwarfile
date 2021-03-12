c64446f26662abd419466eec694d98f6bd83219a
SauravRanswal <66565491+SauravRanswal@users.noreply.github.com>
2021-03-12 18:52:04 +0530

    Add files via upload

---
+++ FarmersBuddy/ignou/src/main/java/com/sauravmajortproject/ignou/IgnouApplication.java
@@ -0,0 +1,14 @@
+package com.sauravmajortproject.ignou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgnouApplication.class, args);
+		System.out.println("Program Started");
+	}
+
+}
---
+++ FarmersBuddy/ignou/src/test/java/com/sauravmajortproject/ignou/IgnouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sauravmajortproject.ignou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgnouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
